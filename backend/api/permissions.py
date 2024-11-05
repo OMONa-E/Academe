0c3c4df25cd36ce0576e1d6af5e7fa9f5a472b86
Omona Emmanuel <omonaemman@gmail.com>
2024-11-05 21:00:29 +0300

    added permissions classes

---
+++ backend/api/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework import permissions
+
+# CEO Auth
+# -------------------------------------
+class IsCEO(permissions.BasePermission):
+    '''
+    Custom permission to only allow users with the role of CEO to Access
+    '''
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role == 'CEO')
+    
+# CEO Or Employer Auth
+# -------------------------------------
+class IsEmployerOrCEO(permissions.BasePermission):
+    '''
+    Custom permission to only allow users with the role of CEO or Employer to Access
+    '''
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role in ['CEO', 'Employer'])
+    
+# CEO or Employer or Employee Auth
+# -------------------------------------
+class IsEmployerOrEmployeeOrCEO(permissions.BasePermission):
+    '''
+    Custom permission to only allow users with the role of CEO or Employer to Access
+    '''
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.role in ['CEO', 'Employee', 'Employer'])
