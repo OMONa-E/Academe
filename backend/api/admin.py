62cc1ecf07c2463400b3a6f155f254f3db37ce56
Omona Emmanuel <omonaemman@gmail.com>
2024-11-05 06:45:22 +0300

    added models to admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here. 
+admin.site.register( (CustomUser, EmployeeProfile, EmployerProfile, Client) )
+admin.site.register( (TrainingModule, ClientProgress, TrainingSession, Payment, Notification, AuditLog) )
