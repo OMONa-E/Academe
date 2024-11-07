efe16d17aeac7a7c581d9e5ec8ee800975a066df
Omona Emmanuel <omonaemman@gmail.com>
2024-11-07 22:59:05 +0300

    added logout view api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,28 @@
-from rest_framework import viewsets, generics, permissions, filters
+from rest_framework import viewsets, generics, permissions, filters, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
 from .serializers import *
 from .permissions import *
 
+
+# Logout API View
+# ------------------------------------------------------------
+class LogoutView(APIView):
+    permission_classes = [ permissions.IsAuthenticated ]
+
+    def post(self, request):
+        try: 
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.check_blacklist()
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # Employer Registration API View
 # ------------------------------------------------------------
