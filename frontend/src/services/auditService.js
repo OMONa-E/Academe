7589b58180aafc120f20b037b650b3dd1fccab5e
Omona Emmanuel <omonaemman@gmail.com>
2024-11-13 19:00:14 +0300

    updated auditservice

--- frontend/src/services/auditService.js
+++ frontend/src/services/auditService.js
@@ -1,1 +1,33 @@
+import axios from 'axios';
+import { getAccessToken } from './authService';
 
+const API_URL = 'http://localhost:8000/api';
+
+// Default headers for Auth
+const authHeaders = () => ({ 
+  headers: { Authorization: `Bearer ${getAccessToken()}`} 
+});
+
+// List - GET_all
+export const getAudits = async () => {
+  const response = await axios.get(`${API_URL}/audit-logs/`, authHeaders());
+  return response.data;
+};
+
+// Retrieve - GET_by_id
+export const getAuditDetails = async (auditId) => {
+  const response = await axios.get(`${API_URL}/audit-logs/${auditId}/`, authHeaders());
+  return  response.data;
+}
+
+// List - GET_all
+export const getActiveSessionAudits = async () => {
+    const response = await axios.get(`${API_URL}/active-sessions/`, authHeaders());
+    return response.data;
+  };
+  
+  // Retrieve - GET_by_id
+  export const getActiveSessionAuditDetails = async (auditId) => {
+    const response = await axios.get(`${API_URL}/active-sessions/${auditId}/`, authHeaders());
+    return  response.data;
+  }
