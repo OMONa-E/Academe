dc3f1eaf3a7e5739bbc0a2b3a5d7f833eb445fb2
Omona Emmanuel <omonaemman@gmail.com>
2024-11-13 19:17:26 +0300

    implemented crud funcs

---
+++ frontend/src/services/employerService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { getAccessToken } from './authService';
+
+const API_URL = 'http://localhost:8000/api';
+
+// Default headers for Auth
+const authHeaders = () => ({ 
+  headers: { Authorization: `Bearer ${getAccessToken()}`} 
+});
+
+// List - GET_all
+export const getEmployer = async () => {
+  const response = await axios.get(`${API_URL}/employers/`, authHeaders());
+  return response.data;
+};
+
+// Retrieve - GET_by_id
+export const getEmployerDetails = async (employerId) => {
+  const response = await axios.get(`${API_URL}/employers/${employerId}/`, authHeaders());
+  return  response.data;
+}
+
+// Update - PUT_by_id
+export const updateEmployer = async (employerId, employerData) => {
+  const response = await axios.put(`${API_URL}/employers/${employerId}/update/`, employerData, authHeaders());
+  return response.data;
+}
+
+// Delete - DELETE_by_id
+export const deleteEmployer = async (employerId) => {
+  const response = await axios.delete(`${API_URL}/employers/${employerId}/delete/`, authHeaders());
+  return response.status === 204;
+}
